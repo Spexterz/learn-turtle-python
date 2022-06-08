e27ffa35d39f81b88840f8ab14fe06e8df1b2e9a
Spexterz <78482365+Spexterz@users.noreply.github.com>
2022-06-08 22:15:22 +0700

    File turtle drawing

---
+++ my_turtle.py
@@ -0,0 +1,38 @@
+import turtle
+tao = turtle.Pen
+tao = turtle.Pen()
+tao.shape("turtle")
+
+def Rectangle():
+    for i in range(4):
+        tao.fd(100)
+        tao.left(90)
+
+def Triangle():
+    tao.fd(100)
+    tao.rt(115)
+    tao.fd(100)
+    tao.rt(122)
+    tao.fd(108)
+    tao.rt(33)
+
+def Circle():
+    for i in range(8):
+        tao.circle(8)
+        tao.rt(45)
+    
+
+def Go(x,y):
+    tao.penup()
+    tao.goto(x,y)
+    tao.pendown()
+
+
+Go(50,50)
+Rectangle()
+Go(-50,-50)
+Triangle()
+Go(-50,50)
+Rectangle()
+Go(15,15)
+Circle()
